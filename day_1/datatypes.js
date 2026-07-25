9bf2a1fa43aec72d176308f14c7dcb2c1368b115
Omkar Patil <omkarsp87@gmail.com>
2026-07-25 13:22:21 +0000

    Your commit message

---
+++ day_1/datatypes.js
@@ -0,0 +1,26 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using the NodeJS not the browser 
+
+// Code readibility should be high 
+console.log( 3 + 3 )
+
+// it should not be 
+console.log( 3 
+    + 3)
+
+// Datatypes
+/* 
+
+number  => 2 to power 53 
+bigint 
+string => " "
+boolean => true , false 
+null => ( null is a stand alone value in JS )
+undefined => value is not defined 
+symbol => unique id
+
+
+
+object => 
+*/
---
+++ day_1/variables.js
@@ -0,0 +1,12 @@
+const accountId = 312 
+var accountEmail = " omkar@gmail.com" 
+let accountPassword = " 031210 "
+accountName = " Omkar "
+ 
+// Note :
+/* prefer not to use var
+because the issue in block scope and function scope */
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountName])
