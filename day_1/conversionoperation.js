7a4208fb0b76a36104545e6ffd8a7b0f036d873d
Omkar Patil <omkarsp87@gmail.com>
2026-07-25 18:28:15 +0000

    Your commit message

---
+++ day_1/conversionoperation.js
@@ -0,0 +1,23 @@
+// we have learned to convert variables from one datatype to another
+
+let numbers = "12"
+
+console.log(typeof numbers);
+
+console.log("after Conversion");
+
+let aftercon = Number(numbers)
+console.log(typeof aftercon);
+
+
+/*
+
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+
+
+*/
+
+
