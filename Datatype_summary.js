21fc98269662d9a43b2bdd34c8d648077976feec
Omkar Patil <omkarsp87@gmail.com>
2026-07-26 15:16:48 +0000

    Your commit message

---
+++ Datatype_summary.js
@@ -0,0 +1,35 @@
+/*
+
+# Primitive Datatype:
+1.String
+2.Number
+3.Boolean
+4.null
+5.undefined 
+6.Symbol
+7.BigInt
+
+# Reference type (Non P rimitive)
+1. Array
+2. Objects
+3. FUnctions
+
+*/
+
+const id = Symbol('123')
+const anotherid = Symbol('123');
+
+console.log(id == anotherid);
+const heros = ["Omkar" , " harsh " , " saurav "];
+let obj = {
+    name : "Omkar ",
+    roll : 2
+}
+
+const myfun = function(){
+    console.log("Hello World");
+    
+}
+
+console.log( heros)
+console.log(obj)
