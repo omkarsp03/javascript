fcd2dd6f666896ca80a10d0d1a8745c17a083951
Omkar Patil <omkarsp87@gmail.com>
2026-07-26 16:24:54 +0000

    Your commit message

---
+++ Basics/strings.js
@@ -0,0 +1,18 @@
+let name = " Omkar "
+let roll = 7
+//modern ways to display 
+console.log(`My Name is ${name} and My roll now is ${roll}`);
+
+
+let myname = new String ( "Omkar Patil")
+console.log(myname);
+console.log(myname.charAt(6));
+console.log(myname.toLowerCase());
+console.log(myname.toUpperCase());
+console.log(myname.length);
+
+const anothername = myname.substring(0,5)
+console.log(anothername);
+
+
+
